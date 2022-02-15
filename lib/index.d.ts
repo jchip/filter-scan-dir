e7de0a290c11c2d7d903ab69c79b7561fde1d744
Joel Chen <joel_chen@intuit.com>
2022-02-14 23:29:38 -0800

    fix type for legacy usage in TS

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,14 @@
 export * from "../dist/index.d";
-import { filterScanDir } from "../dist/index.d";
 
+import { filterScanDir as FSD, filterScanDirSync as FSDS } from "../dist/index.d";
+
+// backward type compatible for legacy require("filter-scan-dir") usage
+// new TS code should do `import {filterScanDir} from "filter-scan-dir"`
+declare function asyncApi(options?: any): Promise<any>;
+declare namespace asyncApi {
+  const sync: (options?: any) => any;
+  const filterScanDir: typeof FSD;
+  const filterScanDirSync: typeof FSDS;
+}
 // @ts-ignore
-export = filterScanDir;
+export = asyncApi;
--- test/spec/index.spec.ts
+++ test/spec/index.spec.ts
@@ -2,13 +2,18 @@
 
 import { expect } from "chai";
 import fsd from "../..";
+import { filterScanDir, filterScanDirSync } from "../..";
 
 describe("commonjs export", function () {
-  it("should have proper exports", () => {
+  it("should have proper exports", async () => {
     expect(fsd).to.be.a("function");
-    expect((fsd as any).sync).to.be.a("function");
+    expect(fsd.sync).to.be.a("function");
     expect(fsd.filterScanDir).to.be.a("function");
     expect(fsd.filterScanDirSync).to.be.a("function");
+    expect(filterScanDir).to.be.a("function");
+    expect(filterScanDirSync).to.be.a("function");
+    expect(fsd.filterScanDir).to.equal(filterScanDir);
+    expect(fsd.filterScanDirSync).to.equal(filterScanDirSync);
   });
 
   it("should scan files", async () => {
